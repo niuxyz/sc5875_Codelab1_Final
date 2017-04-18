c98216a28d7bd8e70b47997e6e31c5dd13b561b6
niuxyz <sc5875@nyu.edu>
2017-04-18 19:23:41 -0400

    Final_v01
    Starting code with fox and clicking! :D

---
+++ sc5875_codelab1_Final/Assets/Script/ClickonAni.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickonAni : MonoBehaviour {
+	private Ray ray;
+	private RaycastHit hit;
+
+	public GameObject animals;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		if (Input.GetMouseButtonDown (0) && Physics.Raycast (ray, out hit) && hit.collider.tag == "Animal") {
+			Debug.Log (hit.collider.gameObject.GetComponent<Animal>().Speak ());
+		}		
+	}
+}
---
+++ sc5875_codelab1_Final/Assets/Script/Fox.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fox : Animal {
+
+//	// Use this for initialization
+//	void Start () {
+//		
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//		
+//	}
+
+	public override string Speak(){
+		return "die kevin!";
+	}
+}
